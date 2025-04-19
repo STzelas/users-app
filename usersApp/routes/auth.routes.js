11e0bd5854e91158b1e2ec49d74d7e1803731fb2
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-19 16:29:48 +0300

    feat: route for google call added and url for google login

--- usersApp/routes/auth.routes.js
+++ usersApp/routes/auth.routes.js
@@ -4,5 +4,8 @@ const router = express.Router()
 const authController = require('../controllers/auth.controller')
 
 router.post('/login', authController.login)
+router.get('/google/callback', authController.googleLogin)
 
 module.exports = router
+
+// https://accounts.google.com/o/oauth2/auth?client_id=825567864650-phqsstcu1t8jkah97q8d8r3100htgsfa.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
