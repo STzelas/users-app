8a41e1a6a9ac92abe0cf9d6c84b9ec7e23cefc33
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-19 16:29:19 +0300

    feat: google login function added

--- usersApp/controllers/auth.controller.js
+++ usersApp/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model')
 const bcrypt = require('bcrypt')
 const authService = require('../services/auth.service')
+const logger = require('../logger/logger')
 
 exports.login = async(request, response) => {
   console.log("Login user", request.body)
@@ -30,6 +31,43 @@ exports.login = async(request, response) => {
     console.log("Problem logging in", err)
     response.status(400).json({
       status:false,
+      data: err
+    })
+  }
+}
+
+exports.googleLogin = async(request, response) => {
+  const code = request.query.code
+
+  try {
+    if(!code) {
+      response.status(400).json({
+        status: false,
+        data: "Authrorization code is missing"
+      })
+      logger.error("Authentication error in google login code request")
+    } else {
+      let user = await authService.googleAuth(code)
+  
+      if(user) {
+        console.log(">>>", user)
+  
+        response.status(200).json({
+          status: true,
+          data: user
+        })
+        logger.info("Successful")
+      } else {
+        response.status(400).json({
+          status: false,
+          data: "Problem in google login"
+        })
+      }
+    }
+  } catch (err) {
+    console.error("Problem with the request", err)
+    response.status(400).json({
+      status: false,
       data: err
     })
   }
